85d1409a4356f067c4ea11d17dbfa22ee77a614d
Ben Therien <cs135cid@yahoo.com>
2019-03-17 18:05:05 -0400

    Create HereStringFromCSV.psm1

---
+++ HereStringFromCSV.psm1
@@ -0,0 +1,12 @@
+﻿Function HereStringFromCSV {
+    param (
+        [parameter (Mandatory=$true)]$HereStringNameToCreate,
+        [parameter (Mandatory=$true)]$SourceCSVFile,
+        [parameter (Mandatory=$true)]$HereStringVARScope
+    )
+    $CSVData = Get-Content $SourceCSVFile | Out-String #Get the Data from the CSV file
+    $HereStringCommand = '$' + "$HereStringNameToCreate" + ' = ' + '@"' + "`n" + $CSVData + '"@' #Build the Here-String command
+    Invoke-Expression $HereStringCommand #Invoke the Here-String command
+    $HereStringCommand = '$' + "$HereStringVARScope" + ':' + "$HereStringNameToCreate" + ' = ' + '$' + "$HereStringNameToCreate" + ' | ConvertFrom-Csv' #Convert the Here-String Array to a CSV type Table 
+    Invoke-Expression $HereStringCommand #Invoke the Here-String command
+}
