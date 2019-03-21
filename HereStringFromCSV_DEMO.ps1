ec227db2390d2f9c2d846da0c2dcb3578d619424
Ben Therien <cs135cid@yahoo.com>
2019-03-20 21:52:49 -0400

    Update HereStringFromCSV_DEMO.ps1

--- HereStringFromCSV_DEMO.ps1
+++ HereStringFromCSV_DEMO.ps1
@@ -7,11 +7,14 @@ $CurrentScriptDir = $PSScriptRoot
 HereStringFromCSV -HereStringNameToCreate SampleVariable -SourceCSVFile $PSScriptRoot\SampleCSV.csv -HereStringVARScope Global
 
 #Display Data
+Write-Host ""
 Write-Host "The entire table comes out as: " -f Red
 $SampleVariable | Format-Table -AutoSize
 
+Write-Host ""
 Write-Host "Now the data from the first column of the CSV can be addressed like so:" -f Red
 $SampleVariable.choice
 
-Write-Host "And jsut like expected, you can pick a piece of data from a certain position like so:" -f Red
+Write-Host ""
+Write-Host "And just like expected, you can pick a piece of data from a certain position like so:" -f Red
 $SampleVariable.choice[7]
